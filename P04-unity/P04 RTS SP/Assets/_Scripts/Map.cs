a70678682108ce32e07a11c199fa1082e201bfb7
LivingValkyrie <deadwynn@gmail.com>
2015-09-28 15:19:50 -0500

    added base classes for map and nodes
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ P04-unity/P04 RTS SP/Assets/_Scripts/Map.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: Map 
+/// </summary>
+public class Map : MonoBehaviour {
+    #region Fields
+
+    MapNode[] nodes = new MapNode[19];
+
+    #endregion
+
+    void Start() { }
+
+    void Update() { }
+
+}
