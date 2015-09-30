5fd5c2f0b0b0ffdaf34e8c83065da86c4f21e10b
deluxturtle <sebaturtle@gmail.com>
2015-09-30 11:22:46 -0500

    Changed Phases to a switch!

--- P04-unity/P04 RTS SP/Assets/_Scripts/ScriptPhases.cs
+++ P04-unity/P04 RTS SP/Assets/_Scripts/ScriptPhases.cs
@@ -23,96 +23,53 @@ public class ScriptPhases : MonoBehaviour {
 	public Text textCurPhase;
 	public Text textNextPhase;
 
-    Phases phase;
+	Phases phase;
 	bool nextPhase = true; //controls when the engine will go to the next phase.
 	int turnNumber = 0;
+    int curPhaseIndex = 0;
+
+    //This will restart the curPhaseIndex to 0 when you reach this num.
+    const int ROTATE = 6;
+
+
+    void Start()
+    {
+        curPhaseIndex = (int)Convert.ChangeType(phase, phase.GetTypeCode());
+        textCurPhase.text = "Current Phase: " + phase.ToString();
+        textNextPhase.text = "Next Phase: " + ((Phases)curPhaseIndex + 1).ToString();
+    }
 
     /// <summary>
     /// @Author: Andrew Seba
-    /// Sets nextPhase = true;
+    /// Increments the phase to next ENUM;
     /// </summary>
-    public void _NextPhase()
+    public void NextPhase()
 	{
-		nextPhase = true;
-	}
-
-
-
-	void Update () {
-
-
-		//GAMESTART
-		if(phase == Phases.GAMESTART && nextPhase)
-		{
-			nextPhase = false;
-			if(turnNumber == 0)
-			{
-                //TODO
-                phase = Phases.ROLL;
-			}
-			else
-			{
-				phase = Phases.ROLL;
-				nextPhase = true;
-			}
-			
-		}
-
-		//ROLL
-		if(phase == Phases.ROLL && nextPhase)
-		{
-			nextPhase = false;
-
-            phase = Phases.TRADE;
-        }
-
-		//TRADE - for future store.
-		if (phase == Phases.TRADE && nextPhase)
-		{
-			nextPhase = false;
-
-			
-			phase = Phases.BUILD;
-
-            nextPhase = true;//remove this when implementing
-        }
-
-		//BUILD
-		if (phase == Phases.BUILD && nextPhase)
-		{
-			nextPhase = false;
-
-			phase = Phases.PROCESS;
-        }
-
-
-		//PROCESS - for future use.
-		if(phase == Phases.PROCESS && nextPhase)
-		{
-			nextPhase = false;
-
-            phase = Phases.ENDGAME;
-            nextPhase = true;//remove this when implementing
-        }
-
-		//ENDGAME
-		if(phase == Phases.ENDGAME && nextPhase)
-		{
-			nextPhase = false;
-            turnNumber++;
-
-            phase = Phases.GAMESTART;
-        }
-
-
-		//update text
+        curPhaseIndex = (curPhaseIndex + 1) % ROTATE;
+        phase = (Phases)curPhaseIndex;
         
-		textCurPhase.text = "Current Phase: " + phase.ToString();
-        int curPhaseIndex = (int)Convert.ChangeType(phase, phase.GetTypeCode());
-        textNextPhase.text = "Next Phase: " + ((Phases)curPhaseIndex + 1).ToString();
 
 
-	}
+        switch (phase)
+        {
+            case (Phases.GAMESTART):
+                break;
+            case (Phases.ROLL):
+                break;
+            case (Phases.TRADE):
+                break;
+            case (Phases.BUILD):
+                break;
+            case (Phases.PROCESS):
+                break;
+            case (Phases.ENDGAME):
+                turnNumber++;
+                break;
+        }
+
+        textCurPhase.text = "Current Phase: " + phase.ToString();
+        textNextPhase.text = "Next Phase: " + ((Phases)((curPhaseIndex + 1) % ROTATE)).ToString();
+    }
 
 
 
