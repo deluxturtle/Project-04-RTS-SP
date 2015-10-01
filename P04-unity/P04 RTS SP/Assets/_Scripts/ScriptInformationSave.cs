cc55c853689f10e4fb808ac5329483f9c12b73b4
deluxturtle <sebaturtle@gmail.com>
2015-10-01 12:33:04 -0500

    Created a gameinformation.txt saving feature.

---
+++ P04-unity/P04 RTS SP/Assets/_Scripts/ScriptInformationSave.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ScriptInformationSave : MonoBehaviour {
+
+
+	public void _SaveInformation(string pName, string pLevelName)
+	{
+		StreamWriter writer = null;
+		string txtInfoLocation = null;
+
+
+		txtInfoLocation = Application.dataPath + "/save_data/" + pLevelName + "/gameinformation.txt";
+
+		using (writer = new StreamWriter(txtInfoLocation))
+		{
+			writer.WriteLine("Name: " + pName);
+			writer.WriteLine("LevelName: " + pLevelName);
+		}
+
+	}
+}
